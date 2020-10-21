1a46eb3d8e5130ebe95319e8d72c3bd546022a29
narvere <beerbee@gmail.com>
2020-10-21 16:08:17 +0300

    init database. make @db

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 #encoding: utf-8
+#https://vimeo.com/105911565
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'seprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
