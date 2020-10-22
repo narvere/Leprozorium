453bc076647661676d9f517571acdcae365a8853
narvere <beerbee@gmail.com>
2020-10-22 15:28:46 +0300

    show latest posts

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ end
 
 get '/' do
 	#выводим посты
-	@results = @db.execut 'select * from Posts order by id desc'
+	@results = @db.execute 'select * from Posts order by id desc'
 	erb :index
 end
 # обработчик get-запроса (браузер получает страницу с сервера)
