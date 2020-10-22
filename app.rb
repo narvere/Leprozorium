0cbded5529f2cc0d9d6b160b5dcef77915f31a50
narvere <beerbee@gmail.com>
2020-10-22 10:38:13 +0300

    add parametr validation

--- app.rb
+++ app.rb
@@ -9,9 +9,12 @@ def init_db
 	@db = SQLite3::Database.new 'seprosorium.db'
 	@db.results_as_hash = true
 end
-
+# configureвызывается каждый раз при конфигарации приложения
+# когда именился код программы и перезагрузилась страница
 configure do
+	#инициализация БД
 	init_db
+	#создает таблицу если таблица не сущестует
 	@db.execute 'CREATE TABLE IF NOT exists Posts
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,21 +22,26 @@ configure do
 		content TEXT
 		)'
 end
-
+#before вызывается каждый раз при перезагрузке
 before do
+	#инициализация БД
   init_db
 end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
-
+# обработчик get-запроса (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
 post '/new' do
+	#получает пересмнную из post-запроса
   content = params[:content]
-
+  if content.length <=0
+		@error =  'Введите текст поста'
+		return erb :new
+	end
 	erb "You typed #{content}"
 end
